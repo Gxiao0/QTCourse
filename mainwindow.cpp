53ea57c7c7219d25436fbe3966befc6d96934ba9
Gxiao0 <181833554+Gxiao0@users.noreply.github.com>
2024-12-10 17:07:55 +0800

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,53 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-void MainWindow::iniUI()
-{
-    fLabCurFile = new QLabel;
-    fLabCurFile->setMidLineWidth(150);
-    fLabCurFile->setText("当前文件：");
-    ui->statusbar->addWidget(fLabCurFile);
-
-    progressBar = new QProgressBar;
-    progressBar -> setMinimum(5);
-    progressBar -> setMaximum(50);
-    progressBar->setValue(ui->textEdit->font().pointSize());
-    ui->statusbar->addWidget(progressBar);
-
-
-    spinFontSize = new QSpinBox;
-    spinFontSize->setMaximum(50);
-    spinFontSize->setMinimum(1);
-    ui->toolBar->addWidget(new QLabel("字体大小"));
-    ui->toolBar->addWidget(spinFontSize);
-    spinFontSize->setFixedSize(70, 20);
-
-    comboFont = new QFontComboBox;
-    ui->toolBar->addWidget(new QLabel("字体"));
-    ui->toolBar->addWidget(comboFont);
-    comboFont->setMinimumSize(100,20);
-
-}
-
-void MainWindow::iniSingalSlots()
-{
-    connect(spinFontSize,SIGNAL(valueChanged(int)),this,
-            SLOT(on_spinBoxFontSize_valueChanged(int)));
-    connect(comboFont,SIGNAL(currentIndexChanged(const QString&)),this,
-            SLOT(on_comboFont_currentIndexChanged(const QString&)));
-    connect(dialog,SIGNAL(triggered(bool)),this,
-            SLOT(on_actAbout_triggered(bool checked)));
-    //connect(button,&QPushButton::clicked,this,&MainWindow::handle);
-}
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    iniUI();
-    iniSingalSlots();
-    setCentralWidget(ui->textEdit);
+
+    m_chatServer=new ChatServer(this);
+
+    connect(m_chatServer,&ChatServer::logMessage,this,&MainWindow::logMessage);
+//当服务器端有日志消息（比如新用户连接等情况）时，对应的消息能传递到MainWindow中进行处理
 }
 
 MainWindow::~MainWindow()
@@ -56,68 +20,26 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_actBold_triggered(bool checked)
+void MainWindow::on_startStopButton_clicked()//启动/停止服务器
 {
-    QTextCharFormat fmt;
-    if(checked)
-        fmt.setFontWeight(QFont::Bold);
-    else
-        fmt.setFontWeight(QFont::Normal);
-    ui->textEdit->mergeCurrentCharFormat(fmt);
-}
-
-
-void MainWindow::on_textEdit_copyAvailable(bool b)
-{
-    ui->actCut->setEnabled(b);
-    ui->actCopy->setEnabled(b);
-    ui->actPaste->setEnabled(ui->textEdit->canPaste());
-
-}
-
-
-void MainWindow::on_textEdit_selectionChanged()
-{
-    QTextCharFormat fmt;
-    fmt = ui->textEdit->currentCharFormat();
-    ui->actItalic->setChecked(fmt.fontItalic());
-    ui->actBold->setChecked(fmt.font().bold());
-    ui->actUnder->setChecked(fmt.fontUnderline());
+    if(m_chatServer->isListening()){//原本在监听该客户端
+        m_chatServer->stopServer();
+        ui->startStopButton->setText("启动服务器");
+        logMessage("服务器已经停止");
+    }
+    else{
+        if(!m_chatServer->listen(QHostAddress::Any,1967)){//启动服务器
+            QMessageBox::critical(this,"错误","无法启动服务器");
+            return;
+        }
+        logMessage("服务器已经启动");
+        ui->startStopButton->setText("停止服务器");
+    }
 
 }
 
-void MainWindow::on_spinBoxFontSize_valueChanged(int aFontSize)
+void MainWindow::logMessage(const QString &msg)
 {
-
-    QTextCharFormat fmt;
-    fmt.setFontPointSize(aFontSize);
-    ui->textEdit->mergeCurrentCharFormat(fmt);
-    progressBar->setValue(aFontSize);
-}
-
-void MainWindow::on_comboFont_currentIndexChanged(const QString &arg1)
-{
-    QTextCharFormat fmt;
-    fmt.setFontFamily(arg1);
-    ui->textEdit->mergeCurrentCharFormat(fmt);
-}
-
-
-
-void MainWindow::on_actAbout_triggered()
-{
-    QMessageBox:: StandardButton result= QMessageBox::information(NULL, "About窗口", "姓名：郭晓纯  学号：2022414040212",QMessageBox::Yes|QMessageBox::No, QMessageBox::Yes);
-    QMessageBox::information(NULL, "OK","You click the OK!");
-        // switch (result)
-        // {
-        //     case QMessageBox::Yes:
-        //         QMessageBox::information(NULL, "YES","You click the yes !");
-        //         break;
-        //     case QMessageBox::No:
-        //         QMessageBox::information(NULL, "No","You click the no !");
-        //         break;
-        //     default:
-        //         break;
-        // }
+    ui->logEditor->appendPlainText(msg);//设置文本框的内容
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,14 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include<QProgressBar>
-#include<QLabel>
-#include<QSpinBox>
-#include<QFontComboBox>
-#include<QDialog>
-#include<QPushButton>
-#include<QVBoxLayout>
-#include<QMessageBox>
+#include "chatserver.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,38 +12,19 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-private:
-    QLabel *fLabCurFile;
-    QProgressBar *progressBar;
-    QSpinBox *spinFontSize;
-    QFontComboBox *comboFont;
-    QDialog *dialog;
-    QVBoxLayout* layout;
-    QPushButton* button;
-    void iniUI();
-    void iniSingalSlots();
-    void handle();
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-
 private slots:
-    void on_actBold_triggered(bool checked);
+    void on_startStopButton_clicked();
 
-    void on_textEdit_copyAvailable(bool b);
-
-    void on_textEdit_selectionChanged();
-
-    void on_spinBoxFontSize_valueChanged(int aFontSize);
-
-    void on_comboFont_currentIndexChanged(const QString &arg1);
-
-
-    void on_actAbout_triggered();
+public slots:
+    void logMessage(const QString &msg);
 
 private:
     Ui::MainWindow *ui;
+
+    ChatServer *m_chatServer;
 };
 #endif // MAINWINDOW_H
