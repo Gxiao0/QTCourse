2c372b3380f66847434e7d236ee9492c89f5fd33
Gxc <14987412+gxc3152689653@user.noreply.gitee.com>
2024-12-17 14:34:24 +0800

    Initial commit

---
+++ ChatServer/serverworker.h
@@ -0,0 +1,33 @@
+#ifndef SERVERWORKER_H
+#define SERVERWORKER_H
+
+#include <QObject>
+#include<QTcpSocket>
+
+class ServerWorker : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ServerWorker(QObject *parent = nullptr);
+    virtual bool setSocketDescriptor(qintptr socketDescriptor);
+    QString userName();
+    void setUserName(QString user);
+
+
+signals:
+    void logMessage(const QString &msg);
+    void jsonReceived(ServerWorker *sender, const QJsonObject &docObj);
+    void disconnectedFromClient();
+
+private:
+    QTcpSocket *m_serverSocket;
+    QString m_userName;
+
+public slots:
+    void onReadyRead();//收到消息
+    void sendMessage(const QString &text, const QString &type="message");//传出去消息
+    void sendJson(const QJsonObject &json);
+
+};
+
+#endif // SERVERWORKER_H
